cddf36648778af0f41ca552fc7a39f38d3ab62dd
--replace-all <victor1996d@gmail.com>
2021-08-23 17:04:33 +0200

    Probados nuevos ejemplos de diccionarios

--- ejercicios_sueltos/diccionarios.py
+++ ejercicios_sueltos/diccionarios.py
@@ -9,18 +9,29 @@ def run():
     # print(mi_diccionario["llave2"])
     # print(mi_diccionario["llave3"])
 
-    poblacion_paises = {
-        "Argentina": 4532764,
-        "Brasil": 6237238346,
-        "Colombia": 6386432
-    }
+    # poblacion_paises = {
+    #     "Argentina": 4532764,
+    #     "Brasil": 6237238346,
+    #     "Colombia": 6386432
+    # }
 
     #print(poblacion_paises["Argentina"])
     # for pais in poblacion_paises.keys():
     #     print(pais)
 
-    for pais, poblacion in poblacion_paises.items():
-        print(pais + " tiene " + str(poblacion) + " habitantes")
+    # for pais, poblacion in poblacion_paises.items():
+    #     print(pais + " tiene " + str(poblacion) + " habitantes")
+
+    my_dict = {
+        'David' : 35,
+        'Erika' : 32,
+        'Jaime' : 50
+    }
+
+    my_dict['Pedro'] = 70
+
+    print(my_dict)
+    
 
 if __name__ == "__main__":
     run()
